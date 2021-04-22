acb31526b8a13591757dbaf5cb6a7468f8b2d669
ArnulfoAguilar <arnulfo.aguilar9405@gmail.com>
2021-04-21 18:30:36 -0600

    agregue texto

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Aprende React
+          Aprende React ponencia
         </a>
       </header>
     </div>
